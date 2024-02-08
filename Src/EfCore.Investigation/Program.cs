f9461d6e822276d9ffe8c6487c26c34cbe8c4360
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-02-08 06:17:19 +0100

    README.md has been updated

--- Src/EfCore.Investigation/Program.cs
+++ Src/EfCore.Investigation/Program.cs
@@ -13,17 +13,6 @@ optionsBuilder.UseNpgsql(connectionString);
 var dbContext = new BudgetSharingDbContext(optionsBuilder.Options);
 dbContext.Database.Migrate();
 
-// Init database by following sql:
-/*
-INSERT INTO "BudgetSharing"."BudgetPermissions"(
-    "Id", "BudgetId", "OwnerId")
-VALUES ('bd4a81f0-96cc-4337-8161-7a57209e9e64', 'f067106a-850f-455a-96ae-a8b5b3a17710', '5efce86f-664c-4064-80b4-84c0b7a6c2b4');
-
-INSERT INTO "BudgetSharing"."Permission"(
-    "BudgetPermissionId", "Id", "ParticipantId", "PermissionType")
-VALUES ('bd4a81f0-96cc-4337-8161-7a57209e9e64', 1, '5efce86f-664c-4064-80b4-84c0b7a6c2b4', 1);
-*/
-
 // CASE 1: Query with SingleOrDefault - Throws InvalidOperationException with message: Sequence contains more than one element
 //var permission = await dbContext.BudgetPermissions.SingleOrDefaultAsync();
 
